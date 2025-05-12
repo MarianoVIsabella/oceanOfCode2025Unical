d6931e09def90e5f681b5c28e9e03ea10d46f411
brunodome <domenico040204@gmail.com>
2025-05-12 15:28:49 +0200

    Modellate in classi ASP move e torpedo

---
+++ src/main/java/com/codingame/game/custom/ASPclasses/Move.java
@@ -0,0 +1,31 @@
+package com.codingame.game.custom.ASPclasses;
+import it.unical.mat.embasp.languages.Id;
+import it.unical.mat.embasp.languages.Param;
+
+@Id("move")
+public class Move {
+
+    @Param(0)
+    private String dir;
+
+    public Move(String dir) {
+        this.dir = dir;
+    }
+
+    public String getDir() {
+        return dir;
+    }
+
+    public void setDir(String dir) {
+        this.dir = dir;
+    }
+
+    @Override
+    public String toString() {
+        return "move(" + dir + ")";
+    }
+
+    public String toUpperString(){
+        return "MOVE " + dir;
+    }
+}
---
+++ src/main/java/com/codingame/game/custom/ASPclasses/Torpedo.java
@@ -0,0 +1,48 @@
+package com.codingame.game.custom.ASPclasses;
+
+import it.unical.mat.embasp.languages.Id;
+import it.unical.mat.embasp.languages.Param;
+
+
+@Id("torpedo")
+public class Torpedo {
+
+    @Param(0)
+    private int X;
+
+    @Param(1)
+    private int Y;
+
+    public int getX() {
+        return X;
+    }
+
+    public void setX(int x) {
+        X = x;
+    }
+
+    public int getY() {
+        return Y;
+    }
+
+    public void setY(int y) {
+        Y = y;
+    }
+
+    public Torpedo(int y, int x) {
+        Y = y;
+        X = x;
+    }
+
+    @Override
+    public String toString() {
+        return "torpedo(" +
+                X + "," +
+                Y +
+                ')';
+    }
+
+    public String toUpperString(){
+        return "TORPEDO" + X + " " + Y;
+    }
+}
